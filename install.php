d9247decef65f0c0834ab7f072327f1cb02b6e3c
Jonas Henrique <@jonas-droid>
2020-10-30 16:35:01 -0300

    Teste Conexão com o banco

--- install.php
+++ install.php
@@ -1,6 +1,10 @@
 <?php
+define("TBUSUARIO", "usuario";)
 
 
-
+require("db.php");
+$sql = "DROP TABLE IF EXIST " . TBUSUARIO;
+banco($sql);
+echo "<p>DB: TABELA " . TBUSUARIO . " apagada.</p>";
 
 ?>
